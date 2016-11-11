caf71b78e8c6767744d60a80c9fba5d8c1fcccd3
Erick Adrian Perez <yo@erickperez.com>
2016-11-10 20:32:22 -0700

    My first commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//notes
+
 var index = require('./routes/index');
 //var users = require('./routes/users');
 //var notes = require('.index/notes');
